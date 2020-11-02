e152591154249a98ffef2ca2ca73d6965eb40a4c
ytakaba <yutakaba@gmail.com>
2020-11-02 17:11:23 +0000

    neural prediction

---
+++ ch03/neural_mnist.py
@@ -0,0 +1,52 @@
+import sys
+import numpy as np
+import pickle
+sys.path.append("/home/ubuntu/workspace/deep-learning-from-scratch")
+from dataset.mnist import load_mnist
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
+def get_date():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("/home/ubuntu/workspace/deep-learning-from-scratch/ch03/sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+    
+    return y
+
+x, t = get_date()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis = 1)
+    
+    accuracy_cnt += np.sum(p == t [i:i+batch_size])
+
+print(accuracy_cnt / len(x))
+    
