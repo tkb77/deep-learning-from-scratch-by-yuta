584979a5244a472db1db880a90b68ef4b59b8430
ytakaba <yutakaba@gmail.com>
2020-11-02 14:55:45 +0000

    show image

---
+++ ch03/mnist_show.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+sys.path.append("/home/ubuntu/workspace/deep-learning-from-scratch")
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.unit8(img))
+    pil_img.save("img.png")
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(label)
+print(img.shape)
+
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
