d2c570ff01dd6206dc5471d64dbb4655ea6cba4f
ytakaba <yutakaba@gmail.com>
2020-11-02 14:57:51 +0000

    fix show img

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -5,7 +5,7 @@ from dataset.mnist import load_mnist
 from PIL import Image
 
 def img_show(img):
-    pil_img = Image.fromarray(np.unit8(img))
+    pil_img = Image.fromarray(np.uint8(img))
     pil_img.save("img.png")
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
