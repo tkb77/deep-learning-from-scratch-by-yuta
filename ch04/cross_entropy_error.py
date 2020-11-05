e85aabe3106b425d3f4d8ce5c3698232f557cfda
ytakaba <yutakaba@gmail.com>
2020-11-05 14:53:16 +0000

    fix cross_entropy_error

--- ch04/cross_entropy_error.py
+++ ch04/cross_entropy_error.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def cross_entropy_error(x):
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(t * np.log(y + delta)) / batch_size
