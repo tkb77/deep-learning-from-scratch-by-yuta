6c80d74729414e887d5fe2ec8774ba86b2ebad51
tkb77 <yutakaba@gmail.com>
2021-01-07 00:22:38 +0900

    finish ch04

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -126,15 +126,16 @@ class TwoLayerNet:
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
-
 # hyper prameter
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+iter_per_epoch = max(train_size / batch_size, 1) #600
 
 network =TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -151,6 +152,13 @@ for i in range(iters_num):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
     
+    #calc per epoch accuracy
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+    
 # show loss image
 fig = plt.figure()
 
@@ -159,3 +167,13 @@ plt.xlabel("iter_num")
 plt.ylabel("loss")
 
 fig.savefig("train_loss_img.png")
+
+# show epoch image
+fig = plt.figure()
+
+plt.plot(train_acc_list, label="train_acc")
+plt.plot(test_acc_list, linestyle="--", label="test_acc")
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+
+fig.savefig("epoch_img.png")
