eb46ade872d64cbade9d4c8bf2a85bb492193e2c
tkb77 <yutakaba@gmail.com>
2021-01-10 15:27:23 +0000

    add MulLayer class

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -13,3 +13,18 @@ class AddLayer:
         dy =  dout * 1
         return dx, dy
         
+class MulLayer:
+    def __init__(self, x, y):
+        self.x = None
+        self.y = None
+    
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y
+        dy =  dout * self.x
+        return dx, dy
