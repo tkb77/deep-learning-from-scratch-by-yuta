3593e61520974e805a4be258da31763c4e71f583
tkb77 <yutakaba@gmail.com>
2021-01-10 14:57:00 +0000

    add AddLayer class

---
+++ ch05/layer_naive.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+class AddLayer:
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy =  dout * 1
+        return dx, dy
+        
