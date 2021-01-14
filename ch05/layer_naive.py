55722890f09aa4eea4ee8545f4b8f0c5b691b33c
tkb77 <yutakaba@gmail.com>
2021-01-14 23:51:04 +0900

    add SoftmaxWithLoss layer

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import numpy as np
+from functions import *
 
 
 class AddLayer:
@@ -78,4 +79,19 @@ class Affine:
         self.db = np.sum(dout, axis=0)
         return dx
 
-
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None # output of softmax
+        self.t = None # training data
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x) 
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]    # バッチの個数
+        dx = (self.y - self.t) / batch_size
+        return dx
