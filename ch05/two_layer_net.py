eb7684df39eb7789540bbaacd218d20310c89b06
tkb77 <yutakaba@gmail.com>
2021-01-17 22:29:34 +0900

    add bp to two_layer_net

--- ch05/two_layer_net.py
+++ ch05/two_layer_net.py
@@ -1,57 +1,10 @@
-import sys
+# coding: utf-8
 import numpy as np
-import pickle
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-sys.path.append("/home/ubuntu/workspace/deep-learning-from-scratch")
-from dataset.mnist import load_mnistc
-
-def cross_entropy_error(y, t):
-    if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-        
-    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
-    if t.size == y.size:
-        t = t.argmax(axis=1)
-             
-    batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
-
-def numerical_gradient(f, x):
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(x)
-    
-    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        idx = it.multi_index
-        tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x) # f(x+h)
-        
-        x[idx] = tmp_val - h 
-        fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        x[idx] = tmp_val # 値を元に戻す
-        it.iternext()   
-        
-    return grad
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def sigmoid_grad(x):
-    return (1.0 - sigmoid(x)) * sigmoid(x)
-
-def softmax(x):
-    x = x - np.max(x, axis=-1, keepdims=True)   # オーバーフロー対策
-    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
-
-
-def get_date():
-    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=False)
-    return x_test, t_test
+from functions import *
+from layer_naive import *
+from collections import OrderedDict
 
 class TwoLayerNet:
 
@@ -62,22 +15,21 @@ class TwoLayerNet:
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
+        
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.Lastlayers = SoftmaxWithLoss()
 
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-    
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-        
-        return y
+        for layers in self.layers.values():
+            x = layers.forward(x)
+        return x
         
     def loss(self, x, t):
         y = self.predict(x)
-        
-        return cross_entropy_error(y, t)
+        return self.Lastlayers.forward(y, t)
     
     def accuracy(self, x, t):
         y = self.predict(x)
@@ -99,30 +51,24 @@ class TwoLayerNet:
         return grads
         
     def gradient(self, x, t):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        grads = {}
-        
-        batch_num = x.shape[0]
-        
         # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        self.loss(x, t)
         
         # backward
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
+        dout = 1
+        dout = self.Lastlayers.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layers in layers:
+            dout = layers.backward(dout)
         
-        dz1 = np.dot(dy, W2.T)
-        da1 = sigmoid_grad(a1) * dz1
-        grads['W1'] = np.dot(x.T, da1)
-        grads['b1'] = np.sum(da1, axis=0)
-
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        
         return grads
-
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
