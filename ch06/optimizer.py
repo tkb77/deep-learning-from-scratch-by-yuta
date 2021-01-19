fd9e9ffcb8eb152b948bc3f36a38fc3d556e2019
tkb77 <yutakaba@gmail.com>
2021-01-19 23:26:34 +0900

    fix Momentum

--- ch06/optimizer.py
+++ ch06/optimizer.py
@@ -16,5 +16,11 @@ class Momentum:
         self.v = None
         
     def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+        
         for key in params.keys():
-            params[key] -= self.lr * grads[key]
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
