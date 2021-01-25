77ae39f0c19a594304d94eb41d3abc86d161906c
tkb77 <yutakaba@gmail.com>
2021-01-25 23:56:14 +0900

    fix batch normalization

--- ch06/batch_norm_test.py
+++ ch06/batch_norm_test.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append("/home/ubuntu/workspace/deep-learning-from-scratch")
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
