d90a14989f02e377f822f78cc08c4e3dbf3995e3
tkb77 <yutakaba@gmail.com>
2021-01-29 15:01:49 +0000

    fix test_im2col

--- ch07/test_im2col.py
+++ ch07/test_im2col.py
@@ -1,7 +1,14 @@
 # coding: utf-8
 import os
 import sys
+import numpy as np
 sys.path.append("/home/ubuntu/workspace/deep-learning-from-scratch")
 from common.util import im2col
 
-x1 = np.random.rand()
+x1 = np.random.rand(1, 3, 7, 7)
+coll = im2col(x1, 5, 5, stride=1, pad=0)
+print(coll.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)
+coll2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(coll2.shape)
